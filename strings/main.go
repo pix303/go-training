cfcf79e5a97f6aa8ff044a766bceaba11da82621
paolo.carraro <paolo.carraro@interlogica.it>
2022-02-08 15:58:58 +0100

    Update with for range and for loop difference

--- strings/main.go
+++ strings/main.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var s = "Ciao 🚨"
+	var s = "Ciao 🚨 - òàè+"
 	var b = []byte(s)
 	var r = []rune(s)
 
-	fmt.Println("----STRING ---------------------------")
+	fmt.Println("----STRING for range---------------------------")
 
 	for k, ss := range s {
-		fmt.Printf("%d: %q - %x - %U \n", k, ss, ss, ss)
+		fmt.Printf("%d: %q - %x - %U -- %#U \n", k, ss, ss, ss, ss)
+	}
+
+	fmt.Println("--------------------------------------")
+	fmt.Println("----STRING for loop ----------------------------")
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%d: %q - %x - %U -- %#U \n", i, s[i], s[i], s[i], s[i])
 	}
 
 	fmt.Println("--------------------------------------")
@@ -22,10 +29,17 @@ func main() {
 	}
 
 	fmt.Println("--------------------------------------")
-	fmt.Println("----RUNES ----------------------------")
+	fmt.Println("----RUNES for range----------------------------")
 
 	for k, rr := range r {
 		fmt.Printf("%d: %q - %x - %U \n", k, rr, rr, rr)
+	}
+
+	fmt.Println("--------------------------------------")
+	fmt.Println("----RUNES for loop ----------------------------")
+
+	for i := 0; i < len(r); i++ {
+		fmt.Printf("%d: %q - %x - %U \n", i, r[i], r[i], r[i])
 	}
 
 	fmt.Println("--------------------------------------")
